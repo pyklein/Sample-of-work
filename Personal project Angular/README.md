a7a875c4155ab43f5ee3d1599f83e610c39bee72
pyklein <pyklein@outlook.fr>
2016-10-26 11:59:13 +1300

    Add angular personnal project
    For the full project send me an email

---
+++ Personal project Angular/README.md
@@ -0,0 +1,14 @@
+# cine-angular
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.15.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for the client side.
+
+Run `node server/server.js` for the server
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ Personal project Angular/app/scripts/controllers/popular.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coursExoApp.controller:PopularCtrl
+ * @description
+ * # PopularCtrl
+ * Controller of the coursExoApp
+ */
+angular.module('coursExoApp')
+  .controller('PopularCtrl', function ($scope, serviceAjax) {
+		$scope.data= {
+			"currentPage":1
+		}
+		$scope.totalPages=0;
+        $scope.orderByPredicate = "title";
+        $scope.orderByReverse = false;
+
+        $scope.loadMovies = function(){
+            $scope.loading = true;
+            serviceAjax.popular($scope.data.currentPage).success(function(data){
+                $scope.loading = false;
+                $scope.movies = data.results;
+                $scope.totalPages = data.total_pages;
+            });
+        };
+
+        $scope.pageChanged = function(){
+            $scope.loadMovies();
+        };
+
+        $scope.clickPredicateName = function(){
+            $scope.orderByReverse = !$scope.orderByReverse;
+            $scope.orderByPredicate = 'title';
+        }
+
+        $scope.clickPredicateRate = function(){
+            $scope.orderByReverse = !$scope.orderByReverse;
+            $scope.orderByPredicate = 'vote_average';
+        }
+
+        $scope.loadMovies();
+  });
